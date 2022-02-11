47855efff4060955d9847dec4f68082840fe9d65
HuongNguyen <92014413+HuongNguyen1989@users.noreply.github.com>
2022-02-11 21:10:28 +0700

    built meaning & result component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Result from "./Result";
 
 export default function Dictionary() {
-  const [keyword, setKeyword] = useState(null);
+  const [keyword, setKeyword] = useState("");
+  const [data, setData] = useState("");
 
   function handleResponse(response) {
-    console.log(response.data);
+    setData(response.data[0]);
   }
 
   function change(event) {
     event.preventDefault();
-    alert(`searching for ${keyword}`);
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
@@ -19,10 +20,11 @@ export default function Dictionary() {
     setKeyword(event.target.value);
   }
   return (
-    <div className="Dictionary mt-5">
+    <div className="Dictionary mt-5 ms-5 me-5">
       <form onSubmit={change}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Result result={data} />
     </div>
   );
 }
---
+++ src/Meaning.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Meaning(props) {
+  return (
+    <div className="Meaning">
+      <h4> {props.meaning.partOfSpeech}</h4>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p className="mt-3">
+              {index + 1} - {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
