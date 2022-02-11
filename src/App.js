bc60036109cb9b71164774363026304c138c056e
HuongNguyen <92014413+HuongNguyen1989@users.noreply.github.com>
2022-02-11 14:37:36 +0700

    created Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header mt-3">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center mt-5">Coded by Huong Nguyen</footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+export default function () {
+  const [keyword, setKeyword] = useState(null);
+
+  function change(event) {
+    event.preventDefault();
+    alert(`searching for ${keyword}`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary mt-5">
+      <form onSubmit={change}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
