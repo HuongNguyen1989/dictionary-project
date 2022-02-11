4f431f95962384ba0aea61b7b42c46fed2928f03
HuongNguyen <92014413+HuongNguyen1989@users.noreply.github.com>
2022-02-11 14:42:17 +0700

    fixed deploy error

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function () {
+export default function Dictionary() {
   const [keyword, setKeyword] = useState(null);
 
   function change(event) {
